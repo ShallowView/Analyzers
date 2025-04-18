088612d9b82f87cc1f183f84fe6f7d06d1fe686b
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-04-18 18:31:56 +0200

    Adapted to new pgn->csv format

--- CSVtoData.py
+++ CSVtoData.py
@@ -1,32 +1,23 @@
 import pandas as pd
 from pandarallel import pandarallel
-from converter.pgn_data import PGNData
 import uuid
 
 pandarallel.initialize(progress_bar=False, verbose=0)
-
-
-def generateCSV(pgn_file: str) -> None:
-    """
-    Generate a CSV file from the PGN file.
-    """
-    pgn = PGNData(pgn_file)
-    pgn.export(moves_required=False)
 
 def createPlayersDataFrame(gameInfo: pd.DataFrame) -> pd.DataFrame:
     """
     Create a DataFrame of players from the game information DataFrame.
     """
-    players = pd.melt(gameInfo, value_vars=["white", "black"], var_name="Color", value_name="PlayerName")
+    players = pd.melt(gameInfo, value_vars=["White", "Black"], var_name="Color", value_name="PlayerName")
     players = players.drop(columns=["Color"])
-    players["Title"] = pd.melt(gameInfo, value_vars=["white_title", "black_title"], var_name="Color", value_name="Title")["Title"].values
+    players["Title"] = pd.melt(gameInfo, value_vars=["WhiteTitle", "BlackTitle"], var_name="Color", value_name="Title")["Title"].values
     players.drop_duplicates(["PlayerName"], inplace=True)
     players["id"] = [str(uuid.uuid4()) for _ in range(len(players))]
 
     # Combine white and black players with their respective Elo ratings
     player_elo = pd.concat([
-        gameInfo[["white", "white_elo"]].rename(columns={"white": "PlayerName", "white_elo": "MaxElo"}),
-        gameInfo[["black", "black_elo"]].rename(columns={"black": "PlayerName", "black_elo": "MaxElo"})
+        gameInfo[["White", "WhiteElo"]].rename(columns={"White": "PlayerName", "WhiteElo": "MaxElo"}),
+        gameInfo[["Black", "BlackElo"]].rename(columns={"Black": "PlayerName", "BlackElo": "MaxElo"})
     ])
 
     # Group by player name and calculate the maximum Elo
@@ -43,23 +34,24 @@ def createGamesDataFrame(gameInfo: pd.DataFrame, players: pd.DataFrame) -> pd.Da
     """
     # Create a DataFrame for the games
     games = pd.DataFrame(columns=["id", "White", "Black", "Result", "WhiteElo", "BlackElo", "Date", "TimeControl", "ECO"], index=[])
-    games["id"] = gameInfo["game_id"]
+    games["id"] = [str(uuid.uuid4()) for _ in range(len(gameInfo))]
+
     player_id_map = players.set_index("PlayerName")["id"].to_dict()
-    games["White"] = gameInfo["white"].parallel_map(player_id_map)
-    games["Black"] = gameInfo["black"].parallel_map(player_id_map)
+    games["White"] = gameInfo["White"].parallel_map(player_id_map)
+    games["Black"] = gameInfo["Black"].parallel_map(player_id_map)
 
     # Map the Result column based on the winner
     games["Result"] = gameInfo.parallel_apply(
-        lambda row: "D" if row["winner"] == "draw" else
-                    "W" if row["winner"] == row["white"] else
-                    "B" if row["winner"] == row["black"] else
+        lambda row: "D" if row["Result"] == "1/2-1/2" else
+                    "W" if row["Result"] == "1-0" else
+                    "B" if row["Result"] == "0-1" else
                     None,
         axis=1
     )
-    games["WhiteElo"] = gameInfo["white_elo"]
-    games["BlackElo"] = gameInfo["black_elo"]
-    games["Date"] = gameInfo["date_created"]
-    games["TimeControl"] = gameInfo["time_control"]
-    games["ECO"] = gameInfo["eco"]
+    games["WhiteElo"] = gameInfo["WhiteElo"]
+    games["BlackElo"] = gameInfo["BlackElo"]
+    games["Date"] = gameInfo["UTCDate"]
+    games["TimeControl"] = gameInfo["TimeControl"]
+    games["ECO"] = gameInfo["ECO"]
 
     return games
