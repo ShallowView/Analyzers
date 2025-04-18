f03be4baa474c4b8c74d10ac6ac4a4b7ab7ac4a2
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-04-18 18:29:57 +0200

    removed game counter

--- __main__.py
+++ __main__.py
@@ -1,19 +1,21 @@
-
-import CSVtoDatabase
 import pandas as pd
 
-from PGNtoData import createPlayersDataFrame
-from PGNtoData import createGamesDataFrame
+from CSVtoData import createPlayersDataFrame
+from CSVtoData import createGamesDataFrame
+from PGNtoCSV import process_pgn
 
 PGNFile = "lichess_elite_2020-06.pgn"
+PlayersCSV = "lichess_elite_2020-06_players.csv"
+GamesCSV = "lichess_elite_2020-06_games.csv"
 
-gameInfo = pd.read_csv("lichess_elite_2020-06_game_info.csv")
+process_pgn(PGNFile, "brut.csv")
+
+gameInfo = pd.read_csv("brut.csv")
 
 players = createPlayersDataFrame(gameInfo)
 
-players.to_csv("lichess_elite_2020-06_players.csv", index=False)
+players.to_csv(PlayersCSV, index=False)
 
 games = createGamesDataFrame(gameInfo, players)
 
 games.to_csv("lichess_elite_2020-06_games.csv", index=False)
-
