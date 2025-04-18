192a74bd0d6ff6f1cadeb51700d2f516353c5d10
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-04-18 18:30:53 +0200

    removed game counter

--- PGNtoCSV.py
+++ PGNtoCSV.py
@@ -1,11 +1,6 @@
-import io
-
 import pandas as pd
 
-
-
 def process_pgn(file : str, output : str) -> None:
-    count = 0
     games = []
     dic = {}
     with open(file, "r") as f:
@@ -17,23 +12,10 @@ def process_pgn(file : str, output : str) -> None:
             else:
                 if len(dic)!=0:  # Append only non-empty dictionaries
                     games.append(dic)
-                    count += 1
                 dic = {}
-    print(f"Number of games: {count}")
 
     # Create a DataFrame
     df = pd.DataFrame(games)
 
     # Save to CSV
     df.to_csv(output, index=False)
-
-
-def count_games(file : io.TextIOBase) -> int:
-    """
-    Count the number of games in a PGN file.
-    """
-    count = 0
-    for line in file:
-        if line.startswith("[Event"):
-            count += 1
-    return count
