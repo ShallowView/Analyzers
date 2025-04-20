2920dba590f459b2f82a274b0d76d136189e69f3
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-04-20 19:20:16 +0200

    Added Postgre connection (game insert not functional)

--- __main__.py
+++ __main__.py
@@ -1,21 +1,40 @@
 import pandas as pd
 
-from CSVtoData import createPlayersDataFrame
-from CSVtoData import createGamesDataFrame
-from PGNtoCSV import process_pgn
+from CSVtoData import *
+from DataToSQL import *
 
-PGNFile = "lichess_elite_2020-06.pgn"
-PlayersCSV = "lichess_elite_2020-06_players.csv"
-GamesCSV = "lichess_elite_2020-06_games.csv"
+# INPUT FILES
+PGNFile = "Datasets/lichess_elite_2020-06.pgn"
+lichessOpeningTSVs = ["Datasets/a.tsv", "Datasets/b.tsv", "Datasets/c.tsv", "Datasets/d.tsv", "Datasets/e.tsv"]
 
-process_pgn(PGNFile, "brut.csv")
+# OUTPUT FILES
+PlayersCSV = "tmp/players.csv"
+GamesCSV = "tmp/games.csv"
+OpeningsCSV = "tmp/openings.csv"
 
-gameInfo = pd.read_csv("brut.csv")
+# Database connection parameters
+db_params = {
+    "dbname": "chess",
+    "user": "docker",
+    "password": "docker",
+    "host": "localhost",
+    "port": 5432
+}
 
-players = createPlayersDataFrame(gameInfo)
+rawPGN = PGNtoDataFrame(PGNFile)
 
+players = createPlayersDataFrame(rawPGN)
+openings = createOpeningsDataFrame(lichessOpeningTSVs)
+games = createGamesDataFrame(rawPGN, players, openings)
+
+# Save DataFrames to CSV files
 players.to_csv(PlayersCSV, index=False)
+games.to_csv(GamesCSV, index=False)
+openings.to_csv(OpeningsCSV, index=False)
 
-games = createGamesDataFrame(gameInfo, players)
+# Connect to the database
+connection = psycopg2.connect(**db_params)
 
-games.to_csv("lichess_elite_2020-06_games.csv", index=False)
+insert_data(connection, "player", players)
+insert_data(connection, "opening", openings)
+# insert_data(connection, "game", games)
