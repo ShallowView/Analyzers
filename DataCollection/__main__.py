f7f94c3af73423b9c0ed028352809445bcab2704
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-04-25 16:00:08 +0200

    Added SSL connection parameters

--- DataCollection/__main__.py
+++ DataCollection/__main__.py
@@ -1,10 +1,15 @@
 from ProcessPGN import *
 
 # INPUT FILES
-PGNFiles = ["Datasets/lichess_elite_2020-06.pgn",
-            "Datasets/lichess_elite_2020-07.pgn",
-            "Datasets/lichess_elite_2020-08.pgn"]
-PGNFile = "Datasets/lichess_elite_2020-09.pgn"
+PGNFiles = ["Datasets/lichess_elite_2024-06.pgn",
+            "Datasets/lichess_elite_2024-07.pgn",
+            "Datasets/lichess_elite_2024-08.pgn"]
+PGNFiles2 = ["Datasets/lichess_elite_2024-09.pgn",
+            "Datasets/lichess_elite_2024-10.pgn",
+            "Datasets/lichess_elite_2024-11.pgn"]
+PGNFiles3 = ["Datasets/lichess_elite_2024-12.pgn",
+            "Datasets/lichess_elite_2025-01.pgn",
+            "Datasets/lichess_elite_2025-02.pgn"]
 
 lichessOpeningTSVs = ["Datasets/a.tsv",
                       "Datasets/b.tsv",
@@ -13,8 +18,20 @@ lichessOpeningTSVs = ["Datasets/a.tsv",
                       "Datasets/e.tsv"]
 
 # Database connection parameters
-db_params = {
-    "dbname": "chess",
+db_params_SSL = {
+    "dbname": "shallowview",
+    "user": "user", # Replace with your database username
+    "password": "password", # Replace with your database password
+    "host": "s0.net.pimous.dev",
+    "port": 31003,
+    "sslmode": "require",  # Enforce SSL connection
+    "sslcert": "path/client-certificate.crt",  # Path to client certificate
+    "sslkey": "path/client-key.key",    # Path to client private key
+    "sslrootcert": "path/CA-certificate.chain.crt"   # Path to CA certificate
+}
+
+db_params_local = {
+    "dbname": "postgres",
     "user": "docker",
     "password": "docker",
     "host": "localhost",
@@ -28,10 +45,11 @@ tables = {
     "openings": "openings"
 }
 
-# Limits total number of cores used in multiprocessing, by default it uses all available cores - 1, reduce if needed
+# Limits total number of cores used in multiprocessing, by default it's ~80% of the cores, reduce if needed
 setMaxCores()
 
-addOpeningsToDatabase(lichessOpeningTSVs, db_params, tables)
+addOpeningsToDatabase(lichessOpeningTSVs, db_params_SSL, tables)
 
-addNewPGNtoDatabase(PGNFiles, db_params, tables)
-addNewPGNtoDatabase([PGNFile], db_params, tables)
+addNewPGNtoDatabase(PGNFiles, db_params_SSL, tables)
+addNewPGNtoDatabase(PGNFiles2, db_params_SSL, tables)
+addNewPGNtoDatabase(PGNFiles3, db_params_SSL, tables)
