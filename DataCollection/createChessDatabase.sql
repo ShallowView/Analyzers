ec4dfd76259fbeb296e314bc5a5cd8775552f0ad
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-07 09:48:27 +0200

    Added current_elo column and associated routine

--- DataCollection/createChessDatabase.sql
+++ DataCollection/createChessDatabase.sql
@@ -8,9 +8,10 @@ CREATE TABLE IF NOT EXISTS players (
     id UUID PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE,
     title VARCHAR(10),
-    max_elo INT
+    max_elo INT,
+    current_elo INT
 );
-ALTER TABLE players OWNER TO SVCollaborator; -- Don't run this if you're creating the tables locally
+
 
 -- Create the openings table
 CREATE TABLE IF NOT EXISTS openings (
@@ -20,7 +21,6 @@ CREATE TABLE IF NOT EXISTS openings (
     pgn TEXT NOT NULL,
     UNIQUE (name, pgn)
 );
-ALTER TABLE openings OWNER TO SVCollaborator; -- Don't run this if you're creating the tables locally
 
 -- Create the games table
 CREATE TABLE IF NOT EXISTS games (
@@ -35,7 +35,6 @@ CREATE TABLE IF NOT EXISTS games (
     opening UUID REFERENCES openings(id),
     UNIQUE (white, black, date_time)
 );
-ALTER TABLE games OWNER TO SVCollaborator; -- Don't run this if you're creating the tables locally
 
 -- Create the function to update the max_elo field in the players table
 CREATE OR REPLACE FUNCTION update_players_max_elo()
@@ -63,3 +62,45 @@ BEGIN
     WHERE players.id = subquery.id;
 END;
 $$ LANGUAGE plpgsql;
+
+-- Create or replace a function to update the current_elo column
+CREATE OR REPLACE FUNCTION update_players_current_elo()
+RETURNS void AS $$
+BEGIN
+    UPDATE players
+    SET current_elo = subquery.elo
+    FROM (
+        SELECT player_id AS id, elo
+        FROM (
+            SELECT player_id, elo, ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY max_date_time DESC) AS row_num
+            FROM (
+                SELECT black AS player_id, black_elo AS elo, max_date_time
+                FROM games
+                JOIN (
+                    SELECT players.id, MAX(games.date_time) AS max_date_time
+                    FROM players
+                    JOIN games ON players.id = games.black
+                    GROUP BY players.id
+                ) AS latest ON games.black = latest.id AND max_date_time = games.date_time
+
+                UNION
+
+                SELECT white AS player_id, white_elo AS elo, max_date_time
+                FROM games
+                JOIN (
+                    SELECT players.id, MAX(games.date_time) AS max_date_time
+                    FROM players
+                    JOIN games ON players.id = games.white
+                    GROUP BY players.id
+                ) AS latest ON games.white = latest.id AND max_date_time = games.date_time
+            ) AS combined
+        ) AS filtered
+        WHERE row_num = 1
+    ) AS subquery
+    WHERE players.id = subquery.id;
+END;
+$$ LANGUAGE plpgsql;
+
+ALTER TABLE players OWNER TO SVCollaborator; -- Don't run this if you're creating the tables locally
+ALTER TABLE openings OWNER TO SVCollaborator; -- Don't run this if you're creating the tables locally
+ALTER TABLE games OWNER TO SVCollaborator; -- Don't run this if you're creating the tables locally
