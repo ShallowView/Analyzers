7d2ec49c74212a912cbbd5f8139ea9f38d8d5e57
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-14 11:46:51 +0200

    Changed to JSON parameter

--- DataCollection/__main__.py
+++ DataCollection/__main__.py
@@ -1,55 +1,46 @@
+import os
+import sys
+
 from __init__ import *
-
-# INPUT FILES
-PGNFiles = ["Datasets/lichess_elite_2024-06.pgn",
-            "Datasets/lichess_elite_2024-07.pgn",
-            "Datasets/lichess_elite_2024-08.pgn"]
-PGNFiles2 = ["Datasets/lichess_elite_2024-09.pgn",
-            "Datasets/lichess_elite_2024-10.pgn",
-            "Datasets/lichess_elite_2024-11.pgn"]
-PGNFiles3 = ["Datasets/lichess_elite_2024-12.pgn",
-            "Datasets/lichess_elite_2025-01.pgn",
-            "Datasets/lichess_elite_2025-02.pgn"]
-
-lichessOpeningTSVs = ["Datasets/a.tsv",
-                      "Datasets/b.tsv",
-                      "Datasets/c.tsv",
-                      "Datasets/d.tsv",
-                      "Datasets/e.tsv"]
-
-# Database connection parameters
-db_params_SSL = {
-    "dbname": "shallowview",
-    "user": "user", # Replace with your database username
-    "password": "password", # Replace with your database password
-    "host": "s0.net.pimous.dev",
-    "port": 31003,
-    "sslmode": "require",  # Enforce SSL connection
-    "sslcert": "path/client-certificate.crt",  # Path to client certificate
-    "sslkey": "path/client-key.key",    # Path to client private key
-    "sslrootcert": "path/CA-certificate.chain.crt"   # Path to CA certificate
-}
-
-db_params_local = {
-    "dbname": "postgres",
-    "user": "docker",
-    "password": "docker",
-    "host": "localhost",
-    "port": 5432
-}
+from json import load
 
 # Names of the tables in the database
 tables = {
-    "games": "games",
-    "players": "players",
-    "openings": "openings"
+	"games": "games",
+	"players": "players",
+	"openings": "openings"
 }
 
-# Limits total number of cores used in multiprocessing, by default it's ~80% of the cores, reduce if needed
-setMaxCores()
+if __name__ == "__main__":
+	def main():
+		args = sys.argv[1:]
+		if not args:
+			raise ValueError("Please provide a JSON file path as an argument.")
+		with open(args[0], 'r') as file:
+			all_params = load(file)
 
-addOpeningsToDatabase(lichessOpeningTSVs, db_params_SSL, tables)
+			db_params = {
+				"dbname": all_params["dbname"],
+				"user": all_params["user"],
+				"password": all_params["password"],
+				"host": all_params["host"],
+				"port": all_params["port"],
+			}
 
-addNewPGNtoDatabase(PGNFiles, db_params_SSL, tables)
-addNewPGNtoDatabase(PGNFiles2, db_params_SSL, tables)
-addNewPGNtoDatabase(PGNFiles3, db_params_SSL, tables)
+			setMaxCores()
+
+			lichessOpeningTSVs = [os.path.join(all_params["openings_dir"], f) for f in
+														os.listdir(all_params["openings_dir"]) if
+														os.path.isfile(
+															os.path.join(all_params["openings_dir"], f))]
+			PGNFiles = [os.path.join(all_params["pgn_files_dir"], f) for f in
+														os.listdir(all_params["pgn_files_dir"]) if
+														os.path.isfile(
+															os.path.join(all_params["pgn_files_dir"], f))]
+
+			addOpeningsToDatabase(lichessOpeningTSVs, db_params, tables)
+
+			addNewPGNtoDatabase(PGNFiles, db_params, tables)
+
+
+	main()
