7d3a768b69aea16fa5cbf8c90fb0745985c51f19
Ndeye Fatou FALL <ndeye.fall2@etudiant.univ-lr.fr>
2025-05-16 15:32:21 +0200

    nouvelle version

--- dataAnalyste/__init__.py
+++ dataAnalyste/__init__.py
@@ -1,1 +1,9 @@
+__author__ = "Ndeye659, SokhnaFaty09"
+__all__ = ["display_statistics", "predict_winner", "analyze_openings", 
+           "insertDataToPostgres", "createOpeningsDataFrame", 
+           "createGamesDataFrame", "createPlayersDataFrame", 
+           "updatePlayersElo", "PGNtoDataFrame"]
+
+
+
 from dataAnalyste.__main__ import get_engine, db_params_SSL
