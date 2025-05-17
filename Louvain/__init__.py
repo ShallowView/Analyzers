e2e41973afa04b9c002fbc5ea782bd4e00f89b8a
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-18 00:57:38 +0200

    Added new imports

--- Louvain/__init__.py
+++ Louvain/__init__.py
@@ -1,4 +1,6 @@
 __author__ = "agueguen-lr"
-__all__ = ["getPlayersOpenings"]
+__all__ = ["getPlayersOpenings", "getNetworkGraph", "plotKamadaKawai",
+					 "plotSpringLayout"]
 
-from getData import getPlayersOpenings
+from getData import getPlayersOpenings, getNetworkGraph
+from visualiseNetwork import plotKamadaKawai, plotSpringLayout
