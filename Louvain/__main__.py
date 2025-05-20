0e05ebf2e5e0a7b82f98d2f36b24d33db9ee4125
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-20 20:41:22 +0200

    Better logging and docs

--- Louvain/__main__.py
+++ Louvain/__main__.py
@@ -19,23 +19,48 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
 
 	# Set up argument parser
-	parser = argparse.ArgumentParser(description="Process input parameters for the Louvain algorithm.")
-	parser.add_argument("-c", "--color", help="Color parameter (required).")
-	parser.add_argument("-l", "--layout", help="Layout parameter (required).")
-	parser.add_argument("json_file", help="Path to the JSON file (required).")
-	parser.add_argument("--min_count", type=int, help="Minimum count (optional).")
-	parser.add_argument("--min_percent", type=float, help="Minimum percentage (optional).")
-	parser.add_argument("--Louvain", type=str, choices=["true", "false"], help="Disable Louvain (optional).")
-	parser.add_argument("--iterations", type=int, help="Number of iterations (optional).")
-	parser.add_argument("--gui", action='store_true', help="Show plot in new window (optional).")
-	
+	parser = argparse.ArgumentParser(
+		description="Create a network graph with chess player-opening data from a "
+								"postgreSQL database, find communities with Louvain's "
+								"algorithm and output to JSON."
+	)
+	parser.add_argument("-c", "--color", type=str,
+											choices=["white", "black"],
+											help="Get data for the players"
+													 " with the white or black pieces (required).")
+	parser.add_argument("-l", "--layout", type=str,
+											choices=["spring", "kamada"],
+											help="How to position the nodes/vertices"
+													 " for the graph (required).")
+	parser.add_argument("json_file", type=str,
+											help="Path to the output JSON file (required).")
+	parser.add_argument("--min_count", type=int,
+											help="Minimum amount of games played by a player"
+													 " in an opening to be included as a node"
+													 " (optional, default = 100).")
+	parser.add_argument("--min_percent", type=float,
+											help="Minimum percentage of games played in an opening"
+													 " by a player compared to total amount of games"
+													 " played by the player before being included"
+													 " as a node (optional, default = 0.05).")
+	parser.add_argument("--Louvain", type=str, choices=["true", "false"],
+											help="Perform partitioning of players into communities"
+													 " with Louvain's algorithm (optional, default = true).")
+	parser.add_argument("--iterations", type=int,
+											help="For layout = 'spring', how many iterations"
+													 " of the Fruchterman-Reingold force-directed algorithm"
+													 " (optional, default = 50).")
+	parser.add_argument("--save", type=str,
+											help="Plot the result and save to a png"
+													 " with the specified name and path (optional).")
+
 	# Parse arguments
 	args = parser.parse_args()
-	
+
 	# Validate JSON file path
 	if not os.path.isfile(args.json_file):
-			raise ValueError(f"File {args.json_file} does not exist.")
-	
+		raise ValueError(f"File {args.json_file} does not exist.")
+
 	# Assign parsed arguments to variables
 	color = args.color
 	layout = args.layout
@@ -43,7 +68,7 @@ if __name__ == "__main__":
 	min_percent = args.min_percent
 	louvain = args.Louvain.lower() == "true" if args.Louvain else None
 	iterations = args.iterations
-	gui = args.gui
+	save = args.save
 
 	with (open(args.json_file, 'r') as file):
 		all_params = load(file)
@@ -63,12 +88,17 @@ if __name__ == "__main__":
 	logger.info("Displaying fetched data:")
 	print(data)
 
+	logger.info("Creating network graph...")
 	graph = getNetworkGraph(data)
 
+	logger.info(
+		f"Calculating node positions for {graph.number_of_nodes()} nodes. "
+		f"This can take a while...")
 	if layout == "kamada":
 		pos = nx.kamada_kawai_layout(graph)
 	elif layout == "spring":
-		pos = nx.spring_layout(graph, iterations=iterations if iterations is not None else 50)
+		pos = nx.spring_layout(graph,
+													 iterations=iterations if iterations is not None else 50)
 	else:
 		raise ValueError("Invalid layout type. Must be 'kamada' or 'spring'.")
 
@@ -76,16 +106,15 @@ if __name__ == "__main__":
 	if louvain is None or louvain:
 		logger.info("Calculating Louvain partitions...")
 		partitions = community_louvain.best_partition(graph)
-		if gui is not None and gui:
-			plotLouvainPartitions(graph, pos, partitions)
+		if save is not None:
+			plotLouvainPartitions(graph, pos, save, partitions)
 	else:
-		if gui is not None and gui:
-			plotBasic(graph, pos, show_edge_labels=False)
-		
-		
+		if save is not None:
+			plotBasic(graph, pos, save, show_edge_labels=False)
+
 	exportPlotToJSON(
-		graph, 
-		pos, 
-		validate_and_extract_params(all_params, ["output"], [""]).get('output'), 
+		graph,
+		pos,
+		validate_and_extract_params(all_params, ["output"], [""]).get('output'),
 		partitions
 	)
